b0d64333a4733964ae424feaeb91f550185c796e
benjiro <majorcaios@gmail.com>
2020-01-22 18:30:14 +0000

    New Template installed

--- wp-content/themes/thanos/index.php
+++ wp-content/themes/thanos/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <?php
-    $files = scandir(bloginfo('template_url')+"/img");
-?>
+//    $files = scandir(bloginfo('template_url')+"/img");
+//?>
 <section class="mt-1">
     <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 px-5">
